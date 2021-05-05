27421794df17e90c916ff16430ea7c93e95ba195
Rajat Nagvenker <rajatnagvenker@gmail.com>
2021-05-05 20:11:38 +0530

    Initial Commit

---
+++ Africe/AfriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfriceApp.swift
+//  Africe
+//
+//  Created by Rajat Nagvenker on 05/05/21.
+//
+
+import SwiftUI
+
+@main
+struct AfriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Africe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Africe
+//
+//  Created by Rajat Nagvenker on 05/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
